49d93a68873f948d0f53334e4abd2cbe773d97af
MekaMdan <m.alencar.v@outlook.com>
2019-05-22 14:03:29 -0300

    struct Message created

--- main.go
+++ main.go
@@ -16,6 +16,16 @@ var clients = make(map[*websocket.Conn]bool)
 //Will act as a queue for messages sent by clients
 var broadcast = make(chan Message)
 
+// object with methods for taking a normal HTTP connection and upgrading to a WebSocket 
+var upgrader = websocket.Upgrader{}
+
+//defines message object
+type Message struct {
+	Email string 'json:"email"'
+	Username string 'json:"username"'
+	Message string 'json:"message"'
+}
+
 func main() {
 
 }
